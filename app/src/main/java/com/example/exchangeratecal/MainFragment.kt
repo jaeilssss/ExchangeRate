a43f00e7625058e5083051e73e760f4c2140d1b0
jaeil <59818827+jaeilssss@users.noreply.github.com>
2022-12-03 19:56:37 +0900

    [feat] UI 레이아웃 구현

--- app/src/main/java/com/example/exchangeratecal/MainFragment.kt
+++ app/src/main/java/com/example/exchangeratecal/MainFragment.kt
@@ -6,6 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import androidx.databinding.DataBindingUtil
+import com.example.exchangeratecal.databinding.MainFragmentBinding
 
 class MainFragment : Fragment() {
 
@@ -15,16 +18,29 @@ class MainFragment : Fragment() {
 
     private lateinit var viewModel: MainViewModel
 
+    private lateinit var binding : MainFragmentBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.main_fragment, container, false)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        binding.model = viewModel
+
+
+
+        var adapter =
+            context?.let { ArrayAdapter(it, R.layout.support_simple_spinner_dropdown_item,resources.getStringArray(R.array.receiver_nation)) }
+        binding.receiverSpinner.adapter =adapter
+
         // TODO: Use the ViewModel
     }
 
